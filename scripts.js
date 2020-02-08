5735519f0796b5ea00c696c75b1576dd97b9531e
TNathanSampson <59663555+TNathanSampson@users.noreply.github.com>
2020-02-07 18:40:51 -0600

    Delete unused spacing

--- scripts.js
+++ scripts.js
@@ -1,28 +1,26 @@
-
 class Player {
-    constructor(deck) {
-        this.deck = [
-        [2, "Two of " + deck],
-        [3, "Three of " + deck],
-        [4, "Four of " + deck], 
-        [5, "Five of " + deck], 
-        [6, "Six of " + deck], 
-        [7, "Seven of " + deck], 
-        [8, "Eight of " + deck], 
-        [9, "Nine of " + deck], 
-        [10, "Ten of " + deck], 
-        [11, "Jack of " + deck], 
-        [12, "Queen of " + deck], 
-        [13, "King of " + deck], 
-        [14, "Ace of " + deck],      
-        ];
-        this.hand = [];
-        this.chips = 0;          
-    }
-} 
+  constructor(deck) {
+    this.deck = [
+      [2, "Two of " + deck],
+      [3, "Three of " + deck],
+      [4, "Four of " + deck],
+      [5, "Five of " + deck],
+      [6, "Six of " + deck],
+      [7, "Seven of " + deck],
+      [8, "Eight of " + deck],
+      [9, "Nine of " + deck],
+      [10, "Ten of " + deck],
+      [11, "Jack of " + deck],
+      [12, "Queen of " + deck],
+      [13, "King of " + deck],
+      [14, "Ace of " + deck]
+    ];
+    this.hand = [];
+    this.chips = 0;
+  }
+}
 
 //Add Test comment
-//Add this as well
 
 const playerSpades = new Player("Spades");
 const playerClubs = new Player("Clubs");
@@ -30,21 +28,21 @@ const playerHearts = new Player("Hearts");
 const playerDiamonds = new Player("Diamonds");
 const playerBully = new Player("Rooks");
 
-function dealHand(Player) {  
-    while (Player.hand.length < 5) {
-        let cardsInDeck = Player.deck.length;
-        function getRandomCard(numOfCards) {
-        return Math.floor(Math.random() * Math.floor(numOfCards));
+function dealHand(Player) {
+  while (Player.hand.length < 5) {
+    let cardsInDeck = Player.deck.length;
+    function getRandomCard(numOfCards) {
+      return Math.floor(Math.random() * Math.floor(numOfCards));
     }
-        let cardNumber = getRandomCard(cardsInDeck);
-        //console.log(cardNumber);            
-        Player.hand.push(Player.deck[cardNumber]);   
-        let removedCard = Player.deck.splice(cardNumber, 1);  
-    }  
+    let cardNumber = getRandomCard(cardsInDeck);
+    //console.log(cardNumber);
+    Player.hand.push(Player.deck[cardNumber]);
+    let removedCard = Player.deck.splice(cardNumber, 1);
+  }
 }
-        
-dealHand(playerSpades); 
 
-console.log(playerSpades.deck); 
+dealHand(playerSpades);
+
+console.log(playerSpades.deck);
 console.log(playerSpades.hand);
 console.log(playerSpades.chips);
